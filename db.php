548371626da69f9ec6cb35240feda4230b6538a5
Victor <victormu2gi@gmail.com>
2023-12-06 18:01:46 +0300

    PHP code integrated for the database for the page findadoctor, CSS styling remaining

--- db.php
+++ db.php
@@ -1,39 +1,50 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> HWS Database</title>
-    </heead>
+        <title>Data Fetcher</title>
+    </head>
     <body>
         <?php
+            
+            
             $servername="localhost";
             $username="root";
             $password="";
+            $db = "hws";
 
             //create connection
-            $conn = msql_connect($servername, $username, $password);
+            $conn = new mysqli($servername, $username, $password, $db);
 
             //check connection
-            if(!$conn){
-                die("Unsuccessful connection:". mysqli_connect_error());
+            if($conn -> connect_error){
+                die("Unsuccessful connection:". $conn -> connect_error );
 
             }
 
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                // Retrieve the value submitted via the form
+                $specialty = $_POST["specialty"];
 
-            $sql = "CREATE DATABASE hws1;";
+                // Prepare and execute the SQL query based on the submitted value
+                $sql = "SELECT * FROM findadoctor WHERE Specialty = '$specialty'";
+                $result = $conn->query($sql);
 
-
-            if(mysqli_query($conn, $sql)){
-                
-                print("Database created successfully");
+                if ($result->num_rows > 0) {
+                    // Output data of each row
+                    while ($row = $result->fetch_assoc()) {
+                        echo "ID: " . $row["FADIndex"] . " - Name: " . $row["Name"] . " - phone number: " . $row["Phone_Number"] . "-email: " . $row["Email"] . " call hours: " . $row["Call_Hours" ] . "<br>";
+                        // Adjust field names (id, name, your_column) based on your table structure
+                    }
+                } else {
+                    echo "No results found.";
+                }
             }
-            else{
+            
+            $conn->close();
+        
 
-                echo "Error creating the database:" , mysqli_error($conn);
-            }
-
-            mysqli_close($conn);
 
         ?>
-
     </body>
+        
 </html>
