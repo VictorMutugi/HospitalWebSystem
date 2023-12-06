910634cfd7f50ad861811be54397e1925a4b1a41
Victor <victormu2gi@gmail.com>
2023-12-06 21:50:14 +0300

    Updates to appointments.php

--- appointments.php
+++ appointments.php
@@ -1,51 +1,65 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Appointment db page</title>
-    </head>
-    <body>
-        <?php
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "hws"; // Replace with your actual database name
+<head>
+    <title>Appointment db page</title>
+</head>
+<body>
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "hws"; // Replace with your actual database name
 
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-        // Prepare data for insertion
-        $fullName = $_POST['FName'];
-        $phoneNumber = $_POST['Number'];
-        $date = $_POST['date'];
-        $time = $_POST['time'];
-        $email = $_POST['mail'];
-        $medic = $_POST['expert'];
-        $issue = $_POST['issue'];
+    // Prepare data for insertion
+    $fullName = $_POST['FName'];
+    $phoneNumber = $_POST['Number'];
+    $date = $_POST['date'];
+    $time = $_POST['time'];
+    $email = $_POST['mail'];
+    $medic = $_POST['expert'];
+    $issue = $_POST['issue'];
 
-        // Generating AIndex and UIIndex
-        $aIndex = uniqid(); // Generate a unique ID for AIndex
-        $uiIndex = "some_value"; // Replace 'some_value' with actual value for UIIndex
+    // Generating AIndex
+    $aIndex = uniqid(); // Generate a unique ID for AIndex
 
-        // SQL query to insert data into table
-        $sql = "INSERT INTO appointments (Full_Name, Email, Date, Time, Medic, Issue, AIndex, UIIndex) 
-                VALUES ('$fullName', '$email', '$date', '$time', '$medic', '$issue', '$aIndex', '$uiIndex')";
+    // SQL query to insert data into table
+    $sql = "INSERT INTO appointments (Full_Name, Email, Date, Time, Medic, Issue, AIndex) 
+            VALUES ('$fullName', '$email', '$date', '$time', '$medic', '$issue', '$aIndex')";
 
-        function redirect($page){
-            header("location: $page");
-            exit();
-        }
-        if ($conn->query($sql) === TRUE) {
-                redirect(clinicalServices.html);
-            } else {
-                echo "Error: " . $sql . "<br>" . $conn->error;
+    if ($conn->query($sql) === TRUE) {
+        // Fetch the inserted data from the database
+        $fetch_sql = "SELECT * FROM appointments WHERE AIndex = '$aIndex'";
+        $result = $conn->query($fetch_sql);
+
+        if ($result !== false && $result->num_rows > 0) {
+            echo "<h2>Data recorded successfully!</h2>";
+            echo "<table border='1' id=appTable>";
+            echo "<tr><th>AIndex</th><th>Name</th><th>Email</th><th>Date</th><th>Time</th><th>Medic</th><th>Issue</th></tr>";
+            
+            // Output data of each row
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr><td>" . $row["AIndex"] . "</td><td>" . $row["Full_Name"] . "</td><td>" . $row["Email"] . "</td><td>" . $row["Date"] . "</td><td>" . $row["Time"] . "</td><td>" . $row["Medic"] . "</td><td>" . $row["Issue"] . "</td></tr>";
             }
+            
+            echo "</table>";
 
-        $conn->close();
-        ?>
-    </body>
+            echo "<a href=\"appTable\" download>Download Receipt</a>";
+        } else {
+            echo "No data found";
+        }
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+    ?>
+</body>
 </html>
